71cf92c3dcf8cbb2af3b684bdfee14cc82cc76ee
Anastasija H <anastasija100991@gmail.com>
2022-06-27 17:05:56 +0100

    adedd python notes folder

---
+++ python_notes/recap.py
@@ -0,0 +1,17 @@
+# this is comment
+# and comments 
+# can be 
+# big
+
+print("This is my file")
+
+greeting= "Hello World"
+print(greeting)
+
+print("This is a string for displaying characters")
+print("1234") #string
+print(1234+1)  #int -whole numb.
+print(12.34) #float
+print(True) #boolean        
+print(False) #boolean
+print(None)   #none-blank/null data
