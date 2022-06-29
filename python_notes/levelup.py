f34b562628d40b1e792d4ba30fa288cb9dc64664
Anastasija H <anastasija100991@gmail.com>
2022-06-29 11:21:29 +0100

    day3 levelup python

--- python_notes/levelup.py
+++ python_notes/levelup.py
@@ -1,5 +1,5 @@
-#python level up
-
+# #python level up
+import random
 print(int(5.6))
 print(type(int("54")))
 
@@ -44,3 +44,63 @@ while name1 not in allowed:
     print("Try again")
     name1=input("What is your name?")
 print("Please come in!")
+
+#########Week1###Day3##################
+
+light = False
+def light_switch():
+    global light
+    if light:
+        print("Whoa! It's bright in here")
+        light = False
+    else:
+        print("Who turned out the lights?")
+        light = True
+light_switch()
+light_switch()
+light_switch()
+light_switch()
+
+#This is the list
+even_nums = [2,4,6,8,10]
+even_nums.append(12)
+even_nums.insert(0,0)
+print(even_nums)
+
+#This is a tuple. It cannot be changed via method...
+# odd_nums = (1,3,5,7,9)
+# odd_nums.append(11) #<----this would not work on the tuple
+# print(odd_nums)
+
+fav_songs = [
+    "Placebo - Happy Birthday in the Sky",
+    "Korn - ADIDAS",
+    "Rammstein - Mein Herz Brennt"
+]
+print(fav_songs[1:3:1]) #[start:stop:step]
+#print(fav_songs[::]) goes through all list
+test = "maxam"
+if test == test[::-1]:
+    print(f"Yes! {test} is a palindrome")
+else:
+    print("It is not a palindrome")
+
+num=random.randint(1,50)
+while num%2==0:
+    print("Even number!")
+    num=random.randint(1,50)
+#if the number is odd, the while loop will never ever run
+print("An odd number!")
+######################################################
+while True:
+    num=random.randint(1,50)
+    print(num)
+    if num%2==0:
+        print("Even number!")
+        continue
+    else:
+        print("An odd number!")
+        break
+#this while loop will always initialise. It mihght go straight to the else/break - but it will have started
+
+
