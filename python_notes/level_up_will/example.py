b07d9b81787aa92b1900056e8fb2015279d87e84
Anastasija H <anastasija100991@gmail.com>
2022-07-04 20:13:55 +0100

    person.py and main.py changes

--- python_notes/level_up_will/example.py
+++ python_notes/level_up_will/example.py
@@ -1,11 +1,19 @@
 class Guitar():
     def __init__(self, colour, string_no):
+        self.colour = colour
         self.string_no = string_no
 
-        def play(self):
-            print("NOISE")
+    def play(self):
+        print("NOISE!")
 
-ana_guitar = Guitar("")
+ana_guitar = Guitar("Blue", 6)
+
+ana_guitar.play()
+
+example_variable_1 = 3.14152
+
+print(type(example_variable_1))
+
 example_variable2 = "Hello World"
 
 print(type(example_variable2))
@@ -20,4 +28,5 @@ some_list = []
 print(type(example_list))
 
 example_list.pop()
+
 print(ana_guitar)
