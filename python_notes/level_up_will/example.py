bd0502428ed2209d691c000dfccd31c2eb9b5f75
Anastasija H <anastasija100991@gmail.com>
2022-07-04 15:01:05 +0100

    mob mode and activitys

---
+++ python_notes/level_up_will/example.py
@@ -0,0 +1,23 @@
+class Guitar():
+    def __init__(self, colour, string_no):
+        self.string_no = string_no
+
+        def play(self):
+            print("NOISE")
+
+ana_guitar = Guitar("")
+example_variable2 = "Hello World"
+
+print(type(example_variable2))
+
+var = 'string example blah blah'
+
+example_list = [
+    1, 2, 3, 4, 5, 6, 7, 8
+]
+
+some_list = []
+print(type(example_list))
+
+example_list.pop()
+print(ana_guitar)
