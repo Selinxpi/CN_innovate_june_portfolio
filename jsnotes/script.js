c3e0930f26ae214d214f69c46681447394573507
Anastasija H <anastasija100991@gmail.com>
2022-07-07 19:26:35 +0100

    dark-mode

---
+++ jsnotes/script.js
@@ -0,0 +1,19 @@
+console.log("Hello World");
+//let --- can chage
+let myName = "Anastasija";
+console.log(myName);
+
+//const----can't change
+const fullName = "Anastasija"
+console.log(fullName);
+
+myName = "Ana";
+console.log(myName);
+
+if (myName==="Anastasija"){
+    console.log("Hello Anastasija")    
+} else if (myName==="Ana"){
+    console.log("Hello Klong")
+}else{
+    console.log("Who are you?")
+}
